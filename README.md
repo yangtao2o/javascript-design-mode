5459893dc9c277ff97a07677664d92b80e7656bd
yangtao <yangtaobanner@foxmail.com>
2019-02-17 00:17:09 +0800

    设计原则

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # javascript-design-mode
 ## 学习《JavaScript设计模式与应用》
 
+* [面向对象](./doc/面向对象.md)
+* [设计原则](./doc/设计原则.md)
--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-// 父类
-class People {
-  constructor(name, age) {
-    this.name = name
-    this.age = age
-  }
-  eat() {
-    alert(`${this.name} eat something`)
-  }
-  speak() {
-    alert(`My name is ${this.name}, age ${this.age}`)
-  }
+function loadImg(src) {
+  let promise = new Promise(function(resolve, reject) {
+    let img = document.createElement('img')
+    img.onload = function() {
+      resolve(img)
+    }
+    img.onerror = function() {
+      reject('图片加载失败')
+    }
+    img.src = src
+  })
+  return promise
 }
 
-// 子类继承父类
-class Student extends People {
-  constructor(name, age, number) {
-    super(name, age)
-    this.number = number
-  }
-  study() {
-    alert(`${this.name} study`)
-  }
-}
-let xiaoming = new Student('xiaoming', 20, 110)
-xiaoming.study()
-xiaoming.eat()
+let src = 'http://img3.mukewang.com/5aee742a0001a53903790379-140-140.jpg'
+let result = loadImg(src)
+
+result.then(function(img) {
+  // part1
+  alert(`width: ${img.width}`)
+  return img
+}).then(function(img) {
+  // part2
+  alert(`height: ${img.height}`)
+}).catch(function(ex) {
+  alert(ex)
+})
+
