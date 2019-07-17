57b4c98f0e8aa427db046b72579e17fcc01104f9
yangtao <istaotao@aliyun.com>
2019-07-17 08:23:39 +0800

    Change files

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 ## 学习慕课网《Javascript 设计模式系统讲解与应用》
 视频资源：[Javascript 设计模式系统讲解与应用](https://coding.imooc.com/class/255.html)
 
-* [面向对象](./doc/面向对象.md)
-* [设计原则](./doc/设计原则.md)
-* [模式概览](./doc/模式概览.md)
+* [面向对象](./doc/video/面向对象.md)
+* [设计原则](./doc/video/设计原则.md)
+* [模式概览](./doc/video/模式概览.md)
 
 ## 《JavaScript 设计模式与开发实践》
 
-* [this-call-apply](./doc/this-call-apply.md)
-* [闭包和高阶函数](./doc/闭包和高阶函数.md)
-* [单例模式](./doc/单例模式.md)
-* [策略模式](./doc/策略模式.md)
+* [this-call-apply](./doc/practice/this-call-apply.md)
+* [闭包和高阶函数](./doc/practice/闭包和高阶函数.md)
+* [单例模式](./doc/practice/单例模式.md)
+* [策略模式](./doc/practice/策略模式.md)
+
+## 《JavaScript 设计模式》
+
+* [面向对象编程](./doc/mode/面向对象编程.md)
--- src/project/index.js
+++ src/project/index.js
@@ -1,45 +1,32 @@
-class Shop {
-  constructor(name) {
-    return this[name].call({})
+class Subject {
+  constructor() {
+    this.status = status;
+    this.observers = [];
   }
-  Steak() {
-    this.time = 30
-    this.price = 20
-    return this
+  getState () { 
+    return this.status 
+  }
+  setState (status) {
+    this.status = status;
+    this.notifyAllObserver = notifyAllObserver();
+  }
+  notifyAllObserver () {
+    this.observers.forEach(observer => {
+      observer.update()
+    });
+  }
+  attatch(observer) {
+    this.observers.push(observer)
   }
 }
-let test = new Shop('Steak')
-console.log(test)
-// 使用 call 调用父构造函数，不仅能够获取父构造函数的属性，而且能返回自构造函数的属性
-function Project(name, price) {
-  this.name = name;
-  this.price = price;
-}
-function Food(name, price) {
-  Project.call(this, name, price);
-  this.categories = 'food'
-}
-let myfood = new Food('noddle', 8);
 
-// 使用call调用匿名函数
-var animals = [
-  { species: 'Lion', name: 'King' },
-  { species: 'Whale', name: 'Fail' }
-];
-for(var i = 0, l = animals.length; i < l; i++) {
-  (function(i) {
-    this.print = function() {
-      console.log(i + ' this is ' + this.species + ', ' + this.name);
-    }
-    this.print();
-  }).call(animals[i], i);
+class Observer {
+  constructor(name, subject) {
+    this.name = name;
+    this.subject = subject;
+    this.subject.attach(this);
+  }
+  update() {
+    
+  }
 }
-// 使用 call 调用函数并指定上下文的this
-var personObj = {
-  name: 'ytao',
-  age: '22'
-}
-function person() {
-  return this.name + this.age
-}
-console.log(person.call(personObj))
