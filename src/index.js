135124da8eb8e3a34c8091aca0f8037742091793
yangtao <istaotao@aliyun.com>
2019-07-31 09:07:55 +0800

    Add mode

--- src/index.js
+++ src/index.js
@@ -1,35 +1,13 @@
-let star = {
-  name: '张三',
-  age: 24,
-  phone: '13600110011'
+var Book = function(title, time, type) {
+  console.log(this instanceof Book)
+  if(this instanceof Book) {
+    this.title = title;
+    this.time = time;
+    this.type = type;
+  } else {
+    return new Book(title, time, type);
+  }
 }
 
-let agent = new Proxy(star, {
-  get: function(target, key) {
-    if (key === 'phone') {
-      return '18909090101'
-    }
-    if (key === 'price') {
-      return 12000
-    }
-    return target[key]
-  },
-  set: function(target, key, val) {
-    if (key === 'customPrice') {
-      if (val < 100000) {
-        throw new Error('价格太低')
-      } else {
-        target[key] === val
-        return true
-      }
-    }
-  }
-})
-
-console.log(agent.name)
-console.log(agent.age)
-console.log(agent.phone)
-console.log(agent.price)
-
-agent.customPrice = 1000
-console.log(agent.customPrice)
+var book = Book('JS', 2019, 'js');
+console.log(book.time)  // 2019
